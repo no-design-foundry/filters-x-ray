70588443b9781a8c67d91d4f16bdd1a8b7adc310
Jan Sindler <jansindl3r@gmail.com>
2024-11-22 01:51:55 +0100

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='x_ray',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     description='no design foundry – x-ray plugin',
     author='Jan Sindler',
